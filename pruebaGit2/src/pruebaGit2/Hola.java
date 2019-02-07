bb82199324c9d08eda708eb7bacf5922a33b9269
aliciarossello <aliciarossello@192.168.34.70>
2019-02-07 19:01:11 +0100

    Primera versió del meu super projecte

---
+++ pruebaGit2/src/pruebaGit2/Hola.java
@@ -0,0 +1,8 @@
+package pruebaGit2;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		System.out.println("Hola GIT");
+	}
+}
---
+++ pruebaGit2/src/pruebaGit2/Hola2.java
@@ -0,0 +1,8 @@
+package pruebaGit2;
+
+public class Hola2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola una altra vegada");
+	}
+}
